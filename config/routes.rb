f8c4bdd50b9b33c665b13752044976f2aa7f66bd
VirAlex <alexou73@gmail.com>
2019-12-17 11:42:17 +0100

    setup user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :path, :as => :update_user_confirmation
+  end
+
+
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmation: 'confirmations'
+  }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
