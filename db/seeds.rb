ebdc43de078f9c458497e6ae5c06e154fbbddf2f
VirAlex <alexou73@gmail.com>
2019-12-17 18:15:50 +0100

    sign in home

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+puts "User destroyed"
+Team.destroy_all
+puts "Teams destroyed"
+Project.destroy_all
+puts "Projects destroyed"
+
+
+
+alex = User.create!(
+  name: "alex",
+  email: "alex@alex.com",
+  password: "azerty"
+  )
+
+
+ team = Team.create!(
+  name: "Team",
+  user_id: alex,
+  )
+
+
+  projet = Project.create!(
+  user: alex,
+  team: team,
+  name: "Ouesh",
+  description: "Yo"
+)
